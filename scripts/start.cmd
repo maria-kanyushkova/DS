083d80a1a1d870e5c35176acfabd70ebd353514c
Maria Kanyushkova <33763743+maria-kanyushkova@users.noreply.github.com>
2021-04-06 16:40:35 +0300

    поправлены замечания #2

--- scripts/start.cmd
+++ scripts/start.cmd
@@ -1,10 +1,10 @@
-start /d ..\Valuator\ dotnet run --no-build --urls "http://localhost:5001"
+start /d ..\Valuator\ dotnet run build --urls "http://localhost:5001"
 start /d ..\Valuator\ dotnet run --no-build --urls "http://localhost:5002"
 
-start /d ..\RankCalculator\ dotnet run --no-build
+start /d ..\RankCalculator\ dotnet run build
 start /d ..\RankCalculator\ dotnet run --no-build
 
-start /d ..\EventLogger\ dotnet run --no-build
-start /d ..\EventLogger\ dotnet run --no-build
+start /d ..\EventsLogger\ dotnet run build
+start /d ..\EventsLogger\ dotnet run --no-build
 
 start /d ..\nginx\ nginx.exe
--- scripts/stop.cmd
+++ scripts/stop.cmd
@@ -1,6 +1,6 @@
 taskkill /f /im valuator.exe
 taskkill /f /im rankCalculator.exe
-taskkill /f /im eventLogger.exe
+taskkill /f /im eventsLogger.exe
 
 cd ..\nginx
 nginx -s stop
