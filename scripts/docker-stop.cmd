e6e629d15362ceeb77458c150440abe4ec219ed4
Maria Kanyushkova <33763743+maria-kanyushkova@users.noreply.github.com>
2021-04-22 19:51:23 +0300

    add pa6 branch and delete old files

--- scripts/docker-stop.cmd
+++
@@ -1,5 +0,0 @@
-﻿docker stop RedisDB & docker rm RedisDB
-docker stop RedisRU & docker rm RedisRU
-docker stop RedisEU & docker rm RedisEU
-docker stop RedisOther & docker rm RedisOther
-docker stop Nats & docker rm Nats
--- scripts/stop.cmd
+++
@@ -1,6 +0,0 @@
-taskkill /f /im valuator.exe
-taskkill /f /im rankCalculator.exe
-taskkill /f /im eventsLogger.exe
-
-cd ..\nginx
-nginx -s stop
